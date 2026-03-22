a4c0f5e7c6d2eb5ce03739013094ac6c99945d74
mgunawardhana <mrgunawardhana27368@gmail.com>
2026-03-22 12:29:10 +0530

    clean architecture basics and how to use functions and private vs public

---
+++ bin/math_utils.dart
@@ -0,0 +1,9 @@
+// private using (_)
+int _add(int a, int b) {
+  return a + b;
+}
+
+//public finctions
+int multiply(int a, int b) {
+  return a * b;
+}
--- bin/my_app.dart
+++ bin/my_app.dart
@@ -1,13 +1,61 @@
+import 'math_utils.dart';
+
 void main() {
-  print('Main Func');
-  sampleFuncOneCalling();
+  List<String> fruits = ["Apple", "Banana"];
+  fruits.add("Mango");
+  print(fruits[1]);
 }
 
-void sampleFuncOneCalling() {
-  sampleFuncTwoCalling();
-  print('Func One');
-}
+// void main() {
+//   int result = add(5, 3);
+//   print(result);
 
-void sampleFuncTwoCalling() {
-  print('sample func 2');
-}
+//   print(multiply(2, 4));
+// }
+
+// void main() {
+//   var amount = calculateDiscount(1000);
+//   print(amount['message']);
+// }
+
+// Map<String, dynamic> calculateDiscount(double price) {
+//   if (1000 < price) {
+//     return {'price': price * 0.9, 'message': 'Discount Applied'};
+//   } else {
+//     return {'price': price, 'message': 'Discount Not Aplied!'};
+//   }
+// }
+
+// bad for scalling ------------------------
+
+// void main() {
+//   int add(int a, int b) {
+//     return a + b;
+//   }
+
+//   print(add(1, 9));
+// }
+
+// -----------------------------------------
+
+// meth 2 ----------------------------------
+
+// void main() {
+//   var count = add(1, 6);
+//   print(count);
+// }
+
+// int add(int a, int b) {
+//   return a + b;
+// }
+
+// meth 2 closed -------------------------
+
+// void sampleFuncOneCalling() {
+//   sampleFuncTwoCalling();
+//   print('Func One');
+// }
+
+// void sampleFuncTwoCalling() {
+//   print('sample func 2');
+// }
